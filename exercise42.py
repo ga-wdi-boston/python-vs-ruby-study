d740d984d59b600471aa2f731305f08cec5f06c8
olivia-leach <olivia.i.m.leach@gmail.com>
2016-05-22 16:29:52 -0400

    Respond to python study

---
+++ exercise42.py
@@ -0,0 +1,76 @@
+## Animal is-a object (yes, sort of confusing) look at the extra credit
+class Animal(object):
+    pass
+
+## Dog is-a Animal
+class Dog(Animal):
+
+    def __init__(self, name):
+        ## Dog has-a name
+        self.name = name
+
+## Cat is-a Animal
+class Cat(Animal):
+
+    def __init__(self, name):
+        ## Cat has-a name
+        self.name = name
+
+## Person is-a object
+class Person(object):
+
+    def __init__(self, name):
+        ## Person has-a name
+        self.name = name
+
+        ## Person has-a pet of some kind
+        self.pet = None
+
+## Employee is-a Person
+class Employee(Person):
+
+    def __init__(self, name, salary):
+        ## ?? hmm what is this strange magic?
+        super(Employee, self).__init__(name)
+        ## Employee has-a salary
+        self.salary = salary
+
+## Fish is-a object
+class Fish(object):
+    pass
+
+## Salmon is-a Fish
+class Salmon(Fish):
+    pass
+
+## Halibut is-a Fish
+class Halibut(Fish):
+    pass
+
+
+## rover is-a Dog
+rover = Dog("Rover")
+
+## Satan is-a Cat
+satan = Cat("Satan")
+
+## Mary is-a Person
+mary = Person("Mary")
+
+## Mary has-a cat named Satan
+mary.pet = satan
+
+## Frank is-a Employee and has-a salary of 120000
+frank = Employee("Frank", 120000)
+
+## Frank has-a Dog named rover
+frank.pet = rover
+
+## flipper is-a Fish
+flipper = Fish()
+
+## crouse is-a Salmon
+crouse = Salmon()
+
+## harry is-a Halibut
+harry = Halibut()
